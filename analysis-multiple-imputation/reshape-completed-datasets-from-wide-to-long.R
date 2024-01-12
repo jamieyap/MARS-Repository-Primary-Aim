6587177b818173241dfc58b02c6434ceafef3468
Jamie Yap <jamieyap@users.noreply.github.com>
2024-01-12 14:29:52 -0500

    Complete basic script to transform wide to long using pivot_longer given column names.

--- analysis-multiple-imputation/reshape-completed-datasets-from-wide-to-long.R
+++ analysis-multiple-imputation/reshape-completed-datasets-from-wide-to-long.R
@@ -10,18 +10,23 @@ dat_timevarying_wide <- readRDS(file = file.path(path_multiple_imputation_pipeli
 
 dat_for_reshaping <- dat_timevarying_wide
 
+list_all <- list()
+
 for(i in 1:length(cols_to_keep_timevarying)){
   current_cols <- paste(cols_to_keep_timevarying[i], 1:60, sep = "_dp")
+  these_cols_only <- c("replicate_id", "participant_id", current_cols)
   
-  dat_long_current_cols <- dat_for_reshaping %>%
-    select("replicate_id", "participant_id", all_of(current_cols)) %>%
+  dat_long_current_cols <- dat_for_reshaping %>% select(all_of(these_cols_only)) %>%
     pivot_longer(
       cols = current_cols,
       names_to = "decision_point",
       names_prefix = cols_to_keep_timevarying[i],
       values_to = cols_to_keep_timevarying[i],
-      values_drop_na = TRUE
+      values_drop_na = FALSE,
     ) %>%
     mutate(decision_point = as.numeric(substring(.[["decision_point"]], first = 4)))
+  
+  list_all <- append(list_all, list(dat_long_current_cols))
 }
 
+dat_reshaped_done <- list_all %>% reduce(left_join, by = join_by("replicate_id", "participant_id", "decision_point"))
