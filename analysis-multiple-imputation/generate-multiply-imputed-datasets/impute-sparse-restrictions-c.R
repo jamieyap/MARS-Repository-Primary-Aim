14bbf53769a555624537f7083b4c1746cce238c2
Jamie Yap <jamieyap@users.noreply.github.com>
2024-01-13 16:13:13 -0500

    Fix bug: use completed dataset from baseline imputation rather than original dataset when forming imputations for decision points that meet sparse restrictions.

--- analysis-multiple-imputation/generate-multiply-imputed-datasets/impute-sparse-restrictions-c.R
+++ analysis-multiple-imputation/generate-multiply-imputed-datasets/impute-sparse-restrictions-c.R
@@ -18,6 +18,8 @@ source("paths.R")
 library(tidyverse)
 library(mice)
 
+# Recall that dat_primary_aim_replicated.rds is an output of the script
+# create-replicated-dataset.R
 dat_long <- readRDS(file = file.path(path_multiple_imputation_pipeline_data, "dat_primary_aim_replicated.rds"))
 dat_long <- dat_long %>%
   group_by(participant_id) %>%
@@ -34,19 +36,30 @@ dat_long <- dat_long %>% filter(decision_point >= 3)
 # for micro-randomization, but not eligible for micro-randomization in the 
 # past 24 hours prior to the current decision point
 ###############################################################################
+
+# STEP 1: Set up dataset for imputation ---------------------------------------
+
 cond1 <- "(eligibility == 1 & eligibility_lag1 == 1 & elig24hrs == 0)"
 cond2 <- "(eligibility == 1 & eligibility_lag1 == 0 & elig24hrs == 1)"
 cond3 <- "(eligibility == 1 & eligibility_lag1 == 0 & elig24hrs == 0)"
 restriction_meet_string <- paste(cond1, cond2, cond3, sep = " | ")
 dat_for_imputation <- dat_long %>% filter(!!rlang::parse_expr(restriction_meet_string))
 
-these_columns <- c("income_val", "is_female", "mdes_pos_mean", "mdes_neg_mean", "gratitude", "baseline_tobacco_history", "srq_mean")
-
 dat_for_imputation <- dat_for_imputation %>%
   select(replicate_id, participant_id, decision_point,
          is_high_effort, is_low_effort,
-         quick_survey_response, Y, cigarette_counts, src_scored,
-         all_of(these_columns))
+         quick_survey_response, Y, cigarette_counts, src_scored)
+
+dat_wide_completed_baseline <- readRDS(file = file.path(path_multiple_imputation_pipeline_data, "sequentially-completed-datasets", mi_dataset_num, "dat_wide_completed_baseline.rds"))
+# Note that the variables specified below would not have missing values anymore
+# since any missing values would have been imputed at a previous step
+historical_vars_to_use <- c("income_val", "is_female", "mdes_pos_mean", "mdes_neg_mean", "gratitude", "baseline_tobacco_history", "srq_mean")
+# Note that we separate out replicate_id and participant_id because we will retain these columns but drop all the variables listed in historical_vars_to_use
+these_columns <- c("replicate_id", "participant_id", historical_vars_to_use)
+dat_baseline_for_merging <- dat_wide_completed_baseline %>% select(all_of(these_columns))
+dat_for_imputation <- left_join(x = dat_for_imputation, y = dat_baseline_for_merging, by = join_by(replicate_id == replicate_id, participant_id == participant_id))
+
+# STEP 2: Specify imputation models -------------------------------------------
 
 my_list <- list()
 LHS <- "quick_survey_response"
@@ -87,6 +100,8 @@ RHS <- paste("baseline_tobacco_history",
              sep = " + ")
 my_list[[LHS]] <- as.formula(paste(LHS, RHS, sep = " ~ "))
 
+# STEP 3: Create completed dataset --------------------------------------------
+
 imp <- mice(data = dat_for_imputation, 
             m = 1, 
             maxit = use_maxit_value,
@@ -99,7 +114,7 @@ dat_for_imputation_completed <- complete(imp, 1)
 ###############################################################################
 # Save
 ###############################################################################
-dat_for_imputation_completed <- dat_for_imputation_completed %>% select(-any_of(these_columns))
+dat_for_imputation_completed <- dat_for_imputation_completed %>% select(-any_of(historical_vars_to_use))
 
 saveRDS(dat_for_imputation_completed, file = file.path(path_multiple_imputation_pipeline_data, "sequentially-completed-datasets", mi_dataset_num, paste("dat_completed_sparse_restrictions", ".rds", sep = "")))
 
